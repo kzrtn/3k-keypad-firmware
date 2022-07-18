684a265cc7a10b1d98d71c63a27637e0dfcc6274
kzrtn <kartistenn2@gmail.com>
2022-07-18 18:42:58 +0800

    Removed build files

--- build/dev_hid_composite/CMakeFiles/dev_hid_composite.dir/home/sdk/pico-sdk/lib/tinyusb/src/class/net/ecm_rndis_device.c.obj.d
+++
@@ -1,4 +0,0 @@
-dev_hid_composite/CMakeFiles/dev_hid_composite.dir/home/sdk/pico-sdk/lib/tinyusb/src/class/net/ecm_rndis_device.c.obj: \
- /home/sdk/pico-sdk/lib/tinyusb/src/class/net/ecm_rndis_device.c \
- /home/sdk/pico-sdk/lib/tinyusb/src/tusb_option.h \
- /home/dev/keypad/dev_hid_composite/tusb_config.h
