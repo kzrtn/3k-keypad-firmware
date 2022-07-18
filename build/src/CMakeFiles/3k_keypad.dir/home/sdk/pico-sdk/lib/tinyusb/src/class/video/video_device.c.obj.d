314f024921a681d8edf3cfa3b3250ef6e6ce2ddb
kzrtn <kartistenn2@gmail.com>
2022-07-18 18:58:58 +0800

    Removed all build files. I swear to god please work this time

--- build/src/CMakeFiles/3k_keypad.dir/home/sdk/pico-sdk/lib/tinyusb/src/class/video/video_device.c.obj.d
+++
@@ -1,4 +0,0 @@
-src/CMakeFiles/3k_keypad.dir/home/sdk/pico-sdk/lib/tinyusb/src/class/video/video_device.c.obj: \
- /home/sdk/pico-sdk/lib/tinyusb/src/class/video/video_device.c \
- /home/sdk/pico-sdk/lib/tinyusb/src/tusb_option.h \
- /home/dev/keypad/src/tusb_config.h
--- build/src/CMakeFiles/3k_keypad_ws2812_pio_h.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for 3k_keypad_ws2812_pio_h.
