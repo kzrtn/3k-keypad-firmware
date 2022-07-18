0838d0826afb11a72fd51a65a22ff85dbffe2ab8
kzrtn <kartistenn2@gmail.com>
2022-07-19 00:27:05 +0800

    Added basic LED lighting

--- src/main.c
+++ src/main.c
@@ -3,11 +3,15 @@
 // - WS2812B initialisation
 // - Basic keypress functionality
 // - Empty functions for multicore usage (haven't been used yet though)
+// - All switch LEDs light up when any key is pressed
+// - Another pio instance for underglow LEDs
 // To do:
-// - Figure out reactive lighting with WS2812B strips
-// - Figure out how to move reactive lightning to second core
+// - Figure out how to get LEDs to light up individually
+// - Figure out how to get underglow LEDs to even work (I AM IN PAIN)
+// - Figure out how to move all lightning to second core
 // - Add 1000hz support
 // - Add debounce
+// - Add lighting colour modes
 // - Add configuration software (somehow...)
 
 #include <stdlib.h>
@@ -25,19 +29,32 @@
 //WS2812B stuff
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
-#include "ws2812.pio.h"
-
-#define IS_RGBW true
-#define NUM_PIXELS 9
-#define WS2812_PIN 26
+#include "generated/ws2812.pio.h"
 
 #include "usb_descriptors.h"
 
-//GPIO pin for switch LEDs is 26
-//GPIO pin for underglow LEDs is 25
 const uint8_t swKey[] = {HID_KEY_Z, HID_KEY_X, HID_KEY_C};
 const uint8_t swGPIO[] = {27, 28, 29};
 const size_t swGPIOsize = 3;
+const int swLEDGPIO = 26;
+const int uLEDGPIO = 25;
+
+//put pixel function
+static inline void put_pixel(uint32_t pixel_grb) {
+  pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
+}
+
+//would this even work? does not seem to be working
+static inline void put_pixel_1(uint32_t pixel_grb) {
+  pio_sm_put_blocking(pio1, 0, pixel_grb << 8u);
+}
+
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
+  return ((uint32_t)(r) << 8) |
+        ((uint32_t)(g) << 16) |
+        (uint32_t)(b);
+}
+
 
 /*------------- INIT -------------*/
 void init() {
@@ -48,13 +65,17 @@ void init() {
     gpio_set_dir(swGPIO[i], GPIO_IN);
     gpio_pull_up(swGPIO[i]);
   }
-
-  // Set up WS2812
+  // Set up switch LEDs
   PIO pio = pio0;
   int sm = 0;
   uint offset = pio_add_program(pio, &ws2812_program);
-  ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, false);
+  ws2812_program_init(pio, sm, offset, swLEDGPIO, 800000, false);
 
+  // Set up underglow LEDs
+  PIO pio_1 = pio1;
+  int sm1 = 1;
+  uint offset1 = pio_add_program(pio_1, &ws2812_program);
+  ws2812_program_init(pio_1, sm1, offset1, uLEDGPIO, 800000, false);
 }
 
 void keyboard() {
@@ -75,12 +96,21 @@ void keyboard() {
         keycode[0] = swKey[i];
 
         tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
-
+        
+        //switch LED lights up when keys are pressed
+        put_pixel(urgb_u32(0xff,0xff,0xff)); //Turn on LED (white)
+        put_pixel(urgb_u32(0xff,0xff,0xff)); //Turn on LED (white)
+        put_pixel(urgb_u32(0xff,0xff,0xff)); //Turn on LED (white)
         isPressed = true;
       }
     }
     // send empty key report if previously has key pressed
-      if(!isPressed) {tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);}
+    if(!isPressed) {tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);}
+
+    // clear LEDs
+    for(int i=0; i<3; i++){
+      put_pixel(urgb_u32(0,0,0)); //Turn off LEDs
+    }
   }
 }
 
@@ -104,6 +134,10 @@ int main(void)
 
   multicore_launch_core1(core1_entry); // Start core 1 - must be called before configuring interrupts
 
+  //turn on underglow LEDs (it's not working though)
+  put_pixel_1(urgb_u32(0xff,0,0)); //Turn on LED (red)
+  put_pixel_1(urgb_u32(0,0xff,0)); //Turn on LED (green)
+  
   while(1) {
     tud_task();
     keyboard();
