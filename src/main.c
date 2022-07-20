b259229150baa2794acd5a55f5cb0e30a4d25693
kzrtn <kartistenn2@gmail.com>
2022-07-21 03:41:43 +0800

    Fixed keypress behaviour and 1000hz

--- src/main.c
+++ src/main.c
@@ -1,16 +1,13 @@
 // 3k keypad for osu!
 // Things added:
-// - Basic keypress functionality (very unoptimised)
-// - Empty functions for multicore usage (haven't been used yet though)
+// - Keypress functionality
+// - 1000hz support
 // - Reactive switch LEDs
 // - Another pio instance for underglow LEDs
 // To do:
-// - Optimise everything help
-// - Figure out how to move all lightning to second core
-// - Add 1000hz support
+// - Figure out how to move all lighting to second core
 // - Add debounce
-// - Add lighting colour modes
-// - Add configuration software (somehow...)
+// - Add configuration software for light modes and custom key config (somehow...)
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -31,13 +28,17 @@
 
 #include "usb_descriptors.h"
 
-const uint8_t swKey[] = {HID_KEY_Z, HID_KEY_X, HID_KEY_C};
+#define swGPIOsize 3 // Number of key switches
+#define swLEDsize 3 // Number of key LEDs
+#define uLEDsize 2 // Number of underglow LEDs
+
+const uint8_t swKeycode[] = {HID_KEY_Z, HID_KEY_X, HID_KEY_C};
+
 const uint8_t swGPIO[] = {27, 28, 29};
-const size_t swGPIOsize = 3;
 const int swLEDGPIO = 26;
 const int uLEDGPIO = 25;
 
-//put pixel function
+// Put pixel function
 static inline void sw_put_pixel(uint32_t pixel_grb) {
   pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
 }
@@ -55,13 +56,14 @@ static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
 
 /*------------- INIT -------------*/
 void init() {
-  //set up button pins
+  // Set up button pins
   for (int i = 0; i < swGPIOsize; i++) {
     gpio_init(swGPIO[i]);
     gpio_set_function(swGPIO[i], GPIO_FUNC_SIO);
     gpio_set_dir(swGPIO[i], GPIO_IN);
     gpio_pull_up(swGPIO[i]);
   }
+
   // Set up switch LEDs
   PIO pio = pio0;
   int sm = 0;
@@ -75,54 +77,54 @@ void init() {
 }
 
 void keyboard() {
-  //poll every 1ms? I don't think this actually works
-  const uint32_t interval_ms = 1;
-  static uint32_t start_ms = 0;
-  static uint8_t ledColorData[9] = {};
-
-  if(board_millis() - start_ms < interval_ms) return;
-  start_ms += interval_ms;
-
   if (tud_hid_ready()) {
     bool isPressed = false;
+    uint8_t keycode[6] = {0};
+    uint8_t keycodeIndex = 0;
 
-    //this is an unoptimised way to deal with the keys, you can't press multiple keys at once
-    for(int i = 0; i < swGPIOsize; i++) {
+    // Fill keycode array
+    // 6 key rollover, a limitation by USB HID, NKRO is possible but... is it even needed?
+    for (uint8_t i = 0; i < swGPIOsize; i++) {
       if(!gpio_get(swGPIO[i])) {
-        //use to avoid send multiple consecutive zero report for keyboard
-        uint8_t keycode[6] = {0};
-        keycode[0] = swKey[i];
-
-        tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
-        
+        keycode[keycodeIndex] = swKeycode[i];
+        keycodeIndex++;
         isPressed = true;
       }
+    }
+      if (isPressed) {
+        // Send key report
+        tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
+      }
+      else {
+        // Send empty report
+        tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);
+      }
 
-      // get button input and convert to byte for next step
+      // Get button input and convert to byte for next step
       uint8_t byte0 = (!gpio_get(swGPIO[0])) ? 0xff : 0x00;
       uint8_t byte1 = (!gpio_get(swGPIO[1])) ? 0xff : 0x00;
       uint8_t byte2 = (!gpio_get(swGPIO[2])) ? 0xff : 0x00;
       uint8_t bytearray[] = {byte0, byte1, byte2};
 
       // Set array and put pixels
+      static uint8_t ledColorData[9] = {};
+
       for(uint8_t ledNr = 0; ledNr < 3; ledNr++)
       {
-        // set LED data array
+        // Set LED data array
         ledColorData[ledNr * 3] = bytearray[ledNr];
         ledColorData[ledNr * 3 + 1] = bytearray[ledNr];
         ledColorData[ledNr * 3 + 2] = bytearray[ledNr];
 
-        // put pixels
+        // Put pixels
         sw_put_pixel(urgb_u32(
           ledColorData[ledNr * 3],
           ledColorData[ledNr * 3 + 1],
           ledColorData[ledNr * 3 + 2]));
       }
     }
-    // send empty key report if previously has key pressed
-    if(!isPressed) {tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);}
   }
-}
+
 
 // Core 1 interrupt handler
 void core1_interrupt_handler() {
@@ -144,7 +146,7 @@ int main(void)
 
   multicore_launch_core1(core1_entry); // Start core 1 - must be called before configuring interrupts
 
-  //turn on underglow LEDs (it's not working though)
+  // Turn on underglow LEDs
   u_put_pixel(urgb_u32(0xff,0xff,0xff)); //Turn on LED (white)
   u_put_pixel(urgb_u32(0xff,0xff,0xff)); //Turn on LED (white)
   
@@ -173,6 +175,7 @@ uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_t
 
 // Invoked when received SET_REPORT control request or
 // received data on OUT endpoint ( Report ID = 0, Type = 0 )
+
 void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize)
 {
   (void) instance;
--- src/usb_descriptors.c
+++ src/usb_descriptors.c
@@ -46,7 +46,7 @@ tusb_desc_device_t const desc_device =
 {
     .bLength            = sizeof(tusb_desc_device_t),
     .bDescriptorType    = TUSB_DESC_DEVICE,
-    .bcdUSB             = USB_BCD,
+    .bcdUSB             = USB_BCD, //USB 2.0 Device?
     .bDeviceClass       = 0x00,
     .bDeviceSubClass    = 0x00,
     .bDeviceProtocol    = 0x00,
@@ -76,7 +76,7 @@ uint8_t const * tud_descriptor_device_cb(void)
 
 uint8_t const desc_hid_report[] =
 {
-  TUD_HID_REPORT_DESC_KEYBOARD( HID_REPORT_ID(REPORT_ID_KEYBOARD))
+  TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(REPORT_ID_KEYBOARD))
 };
 
 // Invoked when received GET HID REPORT DESCRIPTOR
@@ -108,56 +108,8 @@ uint8_t const desc_configuration[] =
   TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
 
   // Interface number, string index, protocol, report descriptor len, EP In address, size & polling interval
-  TUD_HID_DESCRIPTOR(ITF_NUM_HID, 0, HID_ITF_PROTOCOL_NONE, sizeof(desc_hid_report), EPNUM_HID, CFG_TUD_HID_EP_BUFSIZE, 5)
+  TUD_HID_DESCRIPTOR(ITF_NUM_HID, 0, HID_ITF_PROTOCOL_NONE, sizeof(desc_hid_report), EPNUM_HID, CFG_TUD_HID_EP_BUFSIZE, 1)
 };
-
-#if TUD_OPT_HIGH_SPEED
-// Per USB specs: high speed capable device must report device_qualifier and other_speed_configuration
-
-// other speed configuration
-uint8_t desc_other_speed_config[CONFIG_TOTAL_LEN];
-
-// device qualifier is mostly similar to device descriptor since we don't change configuration based on speed
-tusb_desc_device_qualifier_t const desc_device_qualifier =
-{
-  .bLength            = sizeof(tusb_desc_device_qualifier_t),
-  .bDescriptorType    = TUSB_DESC_DEVICE_QUALIFIER,
-  .bcdUSB             = USB_BCD,
-
-  .bDeviceClass       = 0x00,
-  .bDeviceSubClass    = 0x00,
-  .bDeviceProtocol    = 0x00,
-
-  .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
-  .bNumConfigurations = 0x01,
-  .bReserved          = 0x00
-};
-
-// Invoked when received GET DEVICE QUALIFIER DESCRIPTOR request
-// Application return pointer to descriptor, whose contents must exist long enough for transfer to complete.
-// device_qualifier descriptor describes information about a high-speed capable device that would
-// change if the device were operating at the other speed. If not highspeed capable stall this request.
-uint8_t const* tud_descriptor_device_qualifier_cb(void)
-{
-  return (uint8_t const*) &desc_device_qualifier;
-}
-
-// Invoked when received GET OTHER SEED CONFIGURATION DESCRIPTOR request
-// Application return pointer to descriptor, whose contents must exist long enough for transfer to complete
-// Configuration descriptor in the other speed e.g if high speed then this is for full speed and vice versa
-uint8_t const* tud_descriptor_other_speed_configuration_cb(uint8_t index)
-{
-  (void) index; // for multiple configurations
-
-  // other speed config is basically configuration with type = OHER_SPEED_CONFIG
-  memcpy(desc_other_speed_config, desc_configuration, CONFIG_TOTAL_LEN);
-  desc_other_speed_config[1] = TUSB_DESC_OTHER_SPEED_CONFIG;
-
-  // this example use the same configuration for both high and full speed mode
-  return desc_other_speed_config;
-}
-
-#endif // highspeed
 
 // Invoked when received GET CONFIGURATION DESCRIPTOR
 // Application return pointer to descriptor
