c6191e07abaf267021f082edae1b168feef90409
Thomzzz <thomazvanbeeck@gmail.com>
2022-08-02 17:12:58 +0200

    Fix crash after state machine

--- src/main.c
+++ src/main.c
@@ -148,7 +148,6 @@ void core1_entry() {
 int main(void)
 {
   board_init();
-  tusb_init();
   init();
 
   multicore_launch_core1(core1_entry); // Start core 1 - must be called before configuring interrupts
@@ -172,6 +171,9 @@ int main(void)
   u_put_pixel(urgb_u32(0xff,0xff,0xff)); //Turn on LED (white)
   u_put_pixel(urgb_u32(0xff,0xff,0xff)); //Turn on LED (white)
   
+  // Initialize USB after the state machine
+  tusb_init();
+
   while(1) {
     tud_task();
     keyboard();
